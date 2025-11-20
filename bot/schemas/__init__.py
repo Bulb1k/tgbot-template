fa160e15f1cbfc9454197ede81ef4d114464674d
Nazar <95479258+Bulb1k@users.noreply.github.com>
2025-11-20 16:03:17 +0200

    Update __init__.py

--- bot/schemas/__init__.py
+++ bot/schemas/__init__.py
@@ -1,8 +1,4 @@
 from .api_response import ApiResponse, T
-from .orders import Order, OrderCreate, OrderCreateResponse
-from .packets import Packet
-from .payments import Payment, PaymentMethodsResponse
-from .tasks import TaskUndress, TaskUndressCreate
 from .users import User, UserCreate, UserBase, UserUpdate
 
 __all__ = [
@@ -10,14 +6,6 @@ __all__ = [
     "User",
     "UserCreate",
     "UserBase",
-    "PaymentMethodsResponse",
     "T",
     "UserUpdate",
-    "Order",
-    "OrderCreate",
-    "OrderCreateResponse",
-    "Payment",
-    "Packet",
-    "TaskUndress",
-    "TaskUndressCreate",
 ]
